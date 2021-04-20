7800bfdeb047a3b181df1ee7a61c2734b056d53f
Arbaz Qureshi <arbaaz.qureshi789@gmail.com>
2021-04-20 10:51:37 +0530

    email entity for typeOrm created

---
+++ src/controllers/graphQlController.ts
@@ -0,0 +1,9 @@
+// export const graphqlHTTP = ({
+//     schema: querySchema,
+//     rootValue: queryResolver,
+//     graphiql: true,
+//     context: {
+//         req: request,
+//         res: response,
+//     },
+// });
---
+++ src/graphQl/model/email-registration.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsEmail } from 'class-validator';
+import { v4 as uuid } from 'uuid';
+
+@Entity({ name: 'email' })
+export class Email {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @IsEmail()
+    @Column()
+    email: string;
+
+    @Column()
+    isEmailSend: boolean;
+
+    @Column({ nullable: true })
+    createdAt: Date;
+
+    constructor() {
+        this.id = uuid();
+        this.email = '';
+        this.isEmailSend = false;
+        this.createdAt = new Date();
+    }
+}
